2c549ed4cd9b5e7d0fdb4ff33e7851277e738739
FelipeAvelno <felipe.avelino10101@gmail.com>
2025-03-03 18:18:32 -0300

    feat: adiciona aplicações principais

---
+++ src/main/java/com/cdap/climate_api/ClimateApiApplication.java
@@ -0,0 +1,13 @@
+package com.cdap.climate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdap/climate_api/ClimateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdap.climate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
